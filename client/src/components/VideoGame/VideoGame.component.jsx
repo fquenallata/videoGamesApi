1e6e5a5cf6d93146fc36ddedd03de5b8934e70bd
Quena <quenallatafranco@gmail.com>
2023-07-30 12:44:12 -0300

    terminando la parte del contenido de home

--- client/src/components/VideoGame/VideoGame.component.jsx
+++ client/src/components/VideoGame/VideoGame.component.jsx
@@ -1,15 +1,19 @@
 import styles from "./VideoGame.module.css";
+import { Link } from "react-router-dom";
 
 function VideoGame(props) {
-  const { name, image, platforms } = props.videoGame;
+  const { id, name, image, genres } = props.videoGame;
 
   return (
-    <div className={styles.card}>
-      <div className={styles.imageContainer}>
-        <img className={styles.image} src={image} alt={name} />
-        <h6 className={styles.title}>{name}</h6>
+    <Link to={`/detail/${id}`} className={styles.link}>
+      <div className={styles.card}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={image} alt={name} />
+          <h5 className={styles.title}>{name}</h5>
+        </div>
+        <div className={styles.genres}>{genres.join(" | ")}</div>
       </div>
-    </div>
+    </Link>
   );
 }
 
